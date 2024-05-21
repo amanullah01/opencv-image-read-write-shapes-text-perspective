a6503a58d595bdd7b1da9108bbc7494a79262283
Amanullah Aman <amancse.aiub@gmail.com>
2024-05-21 08:27:58 +0600

    Read video from directory

---
+++ read_video.py
@@ -0,0 +1,16 @@
+import cv2
+
+video = cv2.VideoCapture("./videos/2.mp4")
+
+while True:
+    success, frame = video.read()
+
+    if not success:
+        break
+
+    cv2.imshow('Read video from directory', frame)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
