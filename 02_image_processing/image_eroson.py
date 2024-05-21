da7dd68cc8fdf4fd8754ec6b07f51bd932ecbfb9
Amanullah Aman <amancse.aiub@gmail.com>
2024-05-21 09:03:33 +0600

    image erossion

---
+++ 02_image_processing/image_eroson.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+image = cv2.imread('../images/documentscanner2.jpg')
+
+kernel = np.ones((5, 5), np.uint8)
+
+
+cannyEdge = cv2.Canny(image, 400, 500)
+
+dilateImage = cv2.dilate(cannyEdge, kernel, iterations=1)
+
+erosionImage = cv2.erode(dilateImage, kernel, iterations=1)
+
+cv2.imshow('image', image)
+cv2.imshow('EDGE image', cannyEdge)
+cv2.imshow('Dilate image', dilateImage)
+cv2.imshow('Erode image', erosionImage)
+cv2.waitKey(0)
