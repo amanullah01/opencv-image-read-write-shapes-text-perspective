797ac1a103f8219771ebe9b338199410e0fa07c7
Amanullah Aman <amancse.aiub@gmail.com>
2024-05-21 08:20:24 +0600

    read image from directory

---
+++ read_image.py
@@ -0,0 +1,11 @@
+import cv2
+import sys
+print("Python version")
+print(sys.version)
+print("Version info.")
+print(sys.version_info)
+
+
+image = cv2.imread("./images/face.png")
+cv2.imshow("Read Image from Directory", image)
+cv2.waitKey(0)
